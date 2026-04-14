8240251d585b0010729aaf0ce254abb9b04eb9d8
sanjay.s2023csbs@sece.ac.in <sanjay.s2023csbs@sece.ac.in>
2026-04-14 10:04:01 +0530

    reg,login controller successfully completed

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -15,7 +15,46 @@ const  registerUser = async(req,res) =>{
         const {name,email,password} = req.body;
         const userExists = await User.findOne({email});
         if(userExists){
-             return res.status(400).json({message:"User already exists"});
+        return res.status(400).json({message:"User already exists"});
         }
-    }
-}
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+        //create user
+        const user = await User.create({
+            name,
+            email,
+            password:hashedPassword,
+        });
+        res.status(201).json({
+            message : "User registered successfully",
+            token : generateToken(user._id),
+        });
+    }catch(error){
+        res.status(500).json({message:error.message});
+    };
+};
+const LoginUser =  async(req,res)=>{
+        try{
+            const {email,password} = req.body;
+            const user = await User.findOne({email});
+            if(!user){
+                res.status(400).json({message:"User Not Found"});
+            }
+        //compare user
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            res.status(400).json({message:"User is not Valid"});
+        }
+        res.status(200).json({
+            message:"Login Successfully",
+            token : genrateToken(user._id);
+        });
+        }catch(error){
+            res.status(500).json({message:error.message});
+        }
+    };
+module.exports ={
+    registerUser,
+    LoginUser
+};
